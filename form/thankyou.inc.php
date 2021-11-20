b022e514e539acc0d09133afe69950e8300762d7
Emily Person <emilyperson@ufl.edu>
2021-11-20 15:09:46 -0600

    added and styled error.php, thankyou.php

--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -1,3 +1,82 @@
-<h1>Thank you for your enquiry</h1>
-<p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
-<p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+<style type="text/css">
+*{
+    margin: 0;
+    padding: 0;
+    border: 0;
+}
+
+
+
+header{
+    position: fixed;
+    width: 100%;
+    top: 0;
+    background: #efa3a3;
+    padding: 20px;
+}
+
+header a{
+    color: white;
+    text-decoration: none;
+}
+
+header li, nav{
+    display: inline-block;
+    margin: 0 10px;
+    font-family: 'Noto Sans', sans-serif;
+}
+header nav{
+    margin-left: 40px;
+}
+
+.container{
+    max-width: 800px;
+    margin: 0 auto;
+    padding-top: 220px;
+}
+body {
+    background-color: #d9d4d6;
+    color: darkslategray;}
+h1 {
+    font-family: 'Noto Sans', sans-serif;
+    margin: 20px, 0;}
+
+h2 {
+    font-family: 'Noto Sans', sans-serif;
+    margin-bottom: 10px;}
+
+p {
+    font-family: 'Assistant', sans-serif;}
+
+footer {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    background: #efa3a3;
+    padding: 5px;
+    color: white;
+}
+</style>
+
+<header> 
+      <nav>
+        <ul>
+          <li><a href="about.html">About</a></li>
+          <li><a href="recipes.html">Recipes</a></li>
+          <a href="index.html"><img src="images/gb-logo.png" alt="Logo" width="150"></a>
+          <li><a href="wine.html">Wine</a></li>
+          <li><a href="contact.html">Subscribe</a></li>
+        </ul>
+      </nav>
+    </header>
+ 
+    <div class="container">
+
+        <h1>Thank you for your inquiry</h1>
+        <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+        <p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+    </div>
+
+<footer>
+    <p>This page designed by Emily Person</p>
+  </footer>
