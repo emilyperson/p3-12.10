37cac1a7304fdb625252226ef60219550040f3a6
Emily Person <emilyperson@ufl.edu>
2021-11-20 15:01:51 -0600

    added form php folders and styled default.inc.php

---
+++ form/default.inc.php
@@ -0,0 +1,75 @@
+<style type="text/css">
+*{
+    margin: 0;
+    padding: 0;
+    border: 0;
+}
+
+
+
+header{
+    position: fixed;
+    width: 100%;
+    top: 0;
+    background: #efa3a3;
+    padding: 20px;
+}
+
+header a{
+    color: white;
+    text-decoration: none;
+}
+
+header li, nav{
+    display: inline-block;
+    margin: 0 10px;
+    font-family: 'Noto Sans', sans-serif;
+}
+header nav{
+    margin-left: 40px;
+}
+
+.container{
+    max-width: 800px;
+    margin: 0 auto;
+    padding-top: 220px;
+}
+body {
+    background-color: #d9d4d6;
+    color: darkslategray;}
+h1 {
+    font-family: 'Noto Sans', sans-serif;
+    margin: 20px, 0;}
+
+h2 {
+    font-family: 'Noto Sans', sans-serif;
+    margin-bottom: 10px;}
+
+p {
+    font-family: 'Assistant', sans-serif;}
+
+footer {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    background: #efa3a3;
+    padding: 5px;
+    color: white;
+}
+</style>
+
+<header> 
+      <nav>
+        <ul>
+          <li><a href="about.html">About</a></li>
+          <li><a href="recipes.html">Recipes</a></li>
+          <a href="index.html"><img src="images/gb-logo.png" alt="Logo" width="150"></a>
+          <li><a href="wine.html">Wine</a></li>
+          <li><a href="contact.html">Subscribe</a></li>
+        </ul>
+      </nav>
+    </header>
+<div class="container">
+    <h1>Sorry, something went wrong with your submission</h1>
+    <p>Please try again.</p>
+</div>
